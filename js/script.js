9366cc68c837dc6617f107d67c620bc91b667d64
PhiLiNNN <philipp_wendschuch@web.de>
2024-01-21 21:44:55 +0100

    highlight oder icons rdy

---
+++ js/script.js
@@ -0,0 +1,321 @@
+
+let orderAmount = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+
+
+window.addEventListener('resize', handleResize);
+handleResize();
+
+function init() { render(); }
+
+
+function openDropdown() {
+    const dropDownMenu = document.getElementById('droppdown-id');
+    dropDownMenu.classList.toggle('open');
+}
+
+
+function render() {
+    let addToBasket = document.getElementById('order-container-id');
+    load();
+    if (containsOnlyZeros(orderAmount)) { createDishesInfo();  }
+    else {
+        createPayInfo();
+        toggleVisibility('pay-info-container-id', true);
+        orderAmount.forEach((amount, idx) => {
+            if (amount !==0) { addToBasket.innerHTML += templateGenerateShoppingCard(menus[idx], idx, amount); }
+        }) 
+        updateOrderIconCounter();
+    }
+    createPizzaCards('cards-container1-id');
+    createDessertCards('cards-container2-id');
+    createDrinksCards('cards-container3-id');
+}
+
+
+function containsOnlyZeros(list) {
+    for (let i = 0; i < list.length; i++) { if (list[i] !== 0) { return false; } }
+    return true;
+}
+
+function saveDataToLocalStorage(key, data) { localStorage.setItem(key, JSON.stringify(data)); }
+
+
+function save() { saveDataToLocalStorage('amount', orderAmount); }
+
+
+function load() { orderAmount = loadDataFromLocalStorage('amount') ||  Array(data.shortName.length).fill(0); }
+
+
+function loadDataFromLocalStorage(key) {
+    let dataText = localStorage.getItem(key);
+    return JSON.parse(dataText);
+}
+
+
+function generateOrderAmountList() { for (let i = 0; i < data.shortName.length; i++) { orderAmount.push(0); } }
+
+
+function createPizzaCards(box1) {
+    let pizzaCardBox1 = document.getElementById(box1);
+    pizzaCardBox1.innerHTML = '';
+    for (let idx = 0; idx < 6; idx++) {
+        pizzaCardBox1.innerHTML += templatePizzaCard(
+            idx, menus[idx], 
+            data.shortName[idx], 
+            data.topping[idx],
+            data.sauce[idx],
+            data.description[idx],
+            data.price[idx]
+        );
+    }
+}
+function createDessertCards(box1) {
+    let pizzaCardBox1 = document.getElementById(box1);
+    pizzaCardBox1.innerHTML = '';
+    for (let idx = 6; idx < 10; idx++) {
+        pizzaCardBox1.innerHTML += templateDessertCard(
+            idx, menus[idx], 
+            data.shortName[idx], 
+            data.description[idx],
+            data.price[idx]
+        );
+    }
+}
+function createDrinksCards(box1) {
+    let pizzaCardBox1 = document.getElementById(box1);
+    pizzaCardBox1.innerHTML = '';
+    console.log('menus.length',menus.length);
+    for (let idx = 10; idx < menus.length; idx++) {
+        pizzaCardBox1.innerHTML += templateDrinksCard(
+            idx, menus[idx], 
+            data.shortName[idx], 
+            data.description[idx],
+            data.price[idx]
+        );
+    }
+}
+function createDishesInfo() {
+    let addDishesInfo = document.getElementById('order-container-id');
+    addDishesInfo.innerHTML = templateDishesInfo();
+}
+
+
+function createPayInfo() {
+    let price = calPayment();
+    let payInfoCon = document.getElementById('pay-info-container-id');
+    payInfoCon.innerHTML = templatePayInfo(price);
+    console.log(payInfoCon.innerHTML);
+    
+}
+
+
+function toggleVisibility(elementId, show = true) {
+    let element = document.getElementById(elementId);
+    show ? element.classList.remove('d-none') : element.classList.add('d-none');
+}
+
+
+function add(pizzaName, index, addToBasket) {
+    let pizzaId = `pizza${index}`;
+    let existingPizza = isPizzaInBasket(addToBasket, pizzaId);
+    if (containsOnlyZeros(orderAmount)) {
+        createDishesInfo();
+        toggleVisibility('add-dishes-info', false);
+        toggleVisibility('pay-info-container-id', true);
+     
+    }
+    if (!existingPizza) {
+        orderAmount[index] = orderAmount[index] + 1;
+        addToBasket.innerHTML += templateGenerateShoppingCard(pizzaName, index, orderAmount[index]);
+        createPayInfo();
+    
+        
+        
+        save();
+    }
+    else { changeQuantity(1, index); }
+}
+
+
+function getPizzaIndex(index) { return  data.shortName.indexOf(index); }
+
+
+function isPizzaInBasket(addToBasket, pizzaId) { return addToBasket.querySelector(`#${pizzaId}`); }
+
+
+function addPizza(name) {
+    let pizzaIndex = getPizzaIndex(name)
+    let addToBasket = document.getElementById('order-container-id');
+    toggleCardBorderFeedback(`card${pizzaIndex}`);
+    if (pizzaIndex == -1) {return 1;}
+    add(menus[pizzaIndex], pizzaIndex, addToBasket);
+    updateOrderIconCounter();
+}
+
+function toggleCardBorderFeedback(activeCard) { 
+    toggleCardBorderColor(activeCard, true)
+    setTimeout(() => { 
+        toggleCardBorderColor(activeCard, false)
+     }, 500);
+}
+
+
+function calPayment() {
+    let result = 0;
+    orderAmount.forEach((amount, idx) => {
+        result += +data.priceFloat[idx] * amount;
+    });
+    return result.toFixed(2).replace('.', ',');
+}
+
+
+function changeQuantity(increment, idx) {
+    let quantityElement = document.getElementById(`quantity${idx}`);
+    let quantity = +quantityElement.innerText + increment;
+    if (quantity == 0) { 
+        orderAmount[idx] = 0;
+        removeOrder(idx);
+        createPayInfo();
+        save();
+    } 
+    else { updateQuantity(idx, quantity); }
+}
+
+function removeOrder(idx) {
+    let deleteOrder = document.getElementById(`pizza${idx}`);
+    deleteOrder.parentNode.removeChild(deleteOrder);
+    if (orderAmount.every(amount => amount === 0)) { 
+        createDishesInfo();
+        toggleVisibility('add-dishes-info', true);
+        toggleVisibility('pay-info-container-id', false);
+     }
+}
+
+function updateQuantity(idx, quantity) {
+    orderAmount[idx] = quantity;
+    document.getElementById(`quantity${idx}`).innerText = quantity;
+    createPayInfo();
+    save();
+}
+
+
+function toggleCardBorderColor(activeCard, show = true) { 
+    if (show) { document.getElementById(activeCard).classList.add('clicked'); }
+    else { document.getElementById(activeCard).classList.remove('clicked'); }
+}
+    
+function orderSum() { return orderAmount.reduce(function (accumulator, currentNumber) { return accumulator + currentNumber; }, 0); }
+
+
+function handleResize() {
+    let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    let openPopupBg = document.getElementById('mobile-popup-bg-id');
+    let closeButton = document.getElementById('show-close-btn-id');
+    let openButton = document.getElementById('show-open-btn-id');
+    let shoppingCard = document.getElementById('shopping-card-container-id');
+    if (viewportWidth > 1004) {
+        if ((closeButton && !closeButton.classList.contains('d-none')) || (openButton && !openButton.classList.contains('d-none'))) {
+            shoppingCard.style.display = 'flex';
+        }
+    } else {
+        if (shoppingCard && openPopupBg && closeButton) {
+            toggleMobileShoppingCard(false)
+            updateOrderIconCounter();
+            
+        }
+    }
+}
+
+function toggleMobileShoppingCard(open) {
+    let openPopupBg = document.getElementById('mobile-popup-bg-id');
+    let closeButton = document.getElementById('show-close-btn-id');
+    let shoppingCard = document.getElementById('shopping-card-container-id');
+    
+    if (open) {
+        closeButton.classList.remove('d-none');
+        openPopupBg.classList.remove('d-none');
+        shoppingCard.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+    } else {
+        closeButton.classList.add('d-none');
+        openPopupBg.classList.add('d-none');
+        shoppingCard.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    }
+
+    updateOrderIconCounter();
+}
+
+
+
+function updateOrderIconCounter() {
+    let orders = document.getElementById('food-counter-id');
+    let sum = orderSum();
+    orders.innerText = sum;
+}
+
+
+function clearShoppingCard() {
+    data.shortName.forEach((_, index) => {
+        if (orderAmount[index] !== 0) {
+            orderAmount[index] = 0;
+        }
+    });
+    save();
+    openOrderFeedbackPopup();
+}
+
+function openOrderFeedbackPopup() {
+    let feedbackPopup = document.getElementById('order-popup-id');
+    feedbackPopup.classList.remove('d-none');
+    document.body.style.overflow = 'hidden';
+    setTimeout(() => {
+            feedbackPopup.classList.add('d-none');
+            window.location.href = "index.html";
+        }, 3000);
+}
+function deliveryInfo(decision) {
+    
+    let takeAway = document.getElementById('shopping-card-flex-right-id');
+    let delivery = document.getElementById('shopping-card-flex-left-id');
+    let carNone = document.getElementById('car_none');
+    let carHighlight = document.getElementById('car_highlight');
+    let takeAwayNone = document.getElementById('takeAway_none');
+    let takeAwayHighlight = document.getElementById('takeAway_highlight');
+    
+    if (decision == 'delivery')  {
+        if (carHighlight.classList.contains('d-none')) {
+            takeAwayHighlight.classList.add('d-none');
+            takeAwayNone.classList.remove('d-none');
+            takeAway.classList.remove('border-highlight');
+
+            delivery.classList.remove('border-default');
+            delivery.classList.add('border-highlight');
+            carNone.classList.add('d-none');
+            carHighlight.classList.remove('d-none');
+        }
+        else {
+            return;
+        }
+    }
+    else if (decision == 'takeAway') {
+        
+        if (takeAwayHighlight.classList.contains('d-none')) {
+            takeAwayHighlight.classList.remove('d-none');
+            takeAwayNone.classList.add('d-none');
+            takeAway.classList.add('border-highlight');
+
+
+            carNone.classList.remove('d-none');
+            carHighlight.classList.add('d-none');
+            delivery.classList.add('border-default');
+        }
+        else {
+            return;
+        }
+    }
+}
+
+    // border: 2px solid var(--accent-color);
+    // box-shadow: 3px 5px 11px -1px var(--accent-color); 
---
+++ js/template.js
@@ -0,0 +1,146 @@
+
+function templatePayInfo(price) {
+    return /*html*/`
+        <div id="pay-info" class="pay-btn-container">
+            <button onclick="clearShoppingCard()">Bezahlen (${price} €)</button>
+        </div>
+    `;
+}
+
+
+function templateGenerateShoppingCard(name, idx, amount) {
+    return /*html*/`
+        <div id="pizza${idx}" class="order-style">
+            <div class="order-name">
+                <p>${name}</p>
+            </div>
+            <div class="order-btn">
+                <button class="add-amount" onclick="changeQuantity(-1, '${idx}')">-</button>
+                <p id="quantity${idx}"> ${amount} </p>
+                <button class="add-amount" onclick="changeQuantity(1, '${idx}')">+</button>
+            </div>
+        </div>
+   
+    `;
+}
+
+
+function templateDishesInfo() {
+    return /*html*/`
+        <div id="add-dishes-info" class="shopping-card-info">
+            <h2>Fülle deinen Warenkorb</h2>
+            <p>Brich mit dem Gewöhnlichen! Wähle deine<br>Lieblingspizza aus, füge sie dem Warenkorb hinzu und bestelle dein Essen.</p>
+        </div>
+    `;
+}
+
+
+
+function templatePizzaCard(idx, name, shortName, topping, sauce, description, price) {
+    return /*html*/`
+        <input type="radio" name="slide" id="c${idx}" checked>
+        <label id="card${idx}" for="c${idx}" class="card">
+            <div  class="row">
+                <div class="row-coloum">
+                    <div class="icon">${idx}</div>
+                    <h4>${name}</h4>
+                </div>
+                <div class="description">
+                    <img src="./img/${shortName}.png" alt="">
+                    <div class="description-flexrow">
+                        <div class="description-flexrow-center">
+                            <p><span class="highlight">Belag:</span> ${topping}</p>
+                            <p><span class="highlight">Soße:</span> ${sauce}</p>
+                            <p>${description}</p>
+                            <p class="price-color highlight">${price} €</p>
+                        </div>
+                        <div class="add-btn">
+                            <button class="add" onclick="addPizza('${shortName}')">+</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </label>
+    `;
+}
+
+
+function templateDessertCard(idx, name, shortName, description, price) {
+    return /*html*/`
+        <input type="radio" name="slide" id="c${idx}" checked>
+        <label id="card${idx}" for="c${idx}" class="card">
+            <div  class="row">
+                <div class="row-coloum">
+                    <div class="icon">${idx}</div>
+                    <h4>${name}</h4>
+                </div>
+                <div class="description">
+                    <img src="./img/${shortName}.png" alt="">
+                    <div class="description-flexrow">
+                        <div class="description-flexrow-center">
+                            <p>${description}</p>
+                            <p class="price-color highlight">${price} €</p>
+                        </div>
+                        <div class="add-btn">
+                            <button class="add" onclick="addPizza('${shortName}')">+</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </label>
+    `;
+}
+
+function templateDrinksCard(idx, name, shortName, description, price) {
+    return /*html*/`
+        <input type="radio" name="slide" id="c${idx}" checked>
+        <label id="card${idx}" for="c${idx}" class="card">
+            <div  class="row">
+                <div class="row-coloum">
+                    <div class="icon">${idx}</div>
+                    <h4>${name}</h4>
+                </div>
+                <div class="description">
+                    <img src="./img/${shortName}.png" alt="">
+                    <div class="description-flexrow">
+                        <div class="description-flexrow-center">
+                            <p>${description}</p>
+                            <p class="price-color highlight">${price} €</p>
+                        </div>
+                        <div class="add-btn">
+                            <button class="add" onclick="addPizza('${shortName}')">+</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </label>
+    `;
+}
+
+function templatePopup(inputValue, textareaValue, noteID) {
+    return /*html*/`
+            <div class="popup">
+                <input id="tempTitel" type="text"  placeholder="Titel" value="${inputValue}" >
+                <textarea id="tempContent" placeholder="Notiz schreiben...">${textareaValue}</textarea>
+                <button  class="button" onclick="changeNote('${noteID}')">Änderungen speichern</button> 
+            </div>
+        `;
+}
+
+
+function templateNotes(noteID, titel, content, date, time) {
+    return /*html*/`
+            <div id="note${noteID}" class="notes" draggable="true">
+                <input class="input" type="text" value="${titel}">
+                <textarea class="textarea">${content}</textarea>
+                <div class="date-and-Clock-container">
+                    <button class="button" onclick="deleteNote('${noteID}')">Löschen</button> 
+                    <div class="date-and-Clock">
+                        <p>erstellt:</p>
+                        <span>${date}</span>
+                        <span>${time} Uhr</span>
+                    </div> 
+                </div>
+            </div>
+        `;
+}
